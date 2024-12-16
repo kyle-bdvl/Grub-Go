a878a319d03c2425304e181f83c4c8fee78d19a3
Kyle <kyle.boudville@gmail.com>
2024-12-16 21:18:18 +0800

    added a new feature to program

--- Kyle/pageConnector.js
+++ Kyle/pageConnector.js
@@ -1,5 +1,42 @@
-const k14 = document.getElementById('k14Btn');
 
-k14.addEventListener('click', ()=>{
-  window.location.href='mealPage.html';
-});
+function getQueryParam(param){
+  const urlParam = new URLSearchParams(window.location.search);
+  return urlParam.get(param);
+}
+
+function setupNavigationButtons(){
+  const buttons = document.querySelectorAll('.action-btn');
+  buttons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+      const targetDivId = button.getAttribute('data-target-div');
+      window.location.href = `mealPage.html?target=${targetDivId}`;
+    });
+  });
+}
+
+function showTargetDiv(){
+  const targetDivId = getQueryParam('target');
+  const allDivs = document.querySelectorAll('.content');
+
+  allDivs.forEach(div =>(div.style.display ='none'));
+  
+
+  if (targetDivId){
+    const targetDiv = document.querySelector(`[data-target-div="${targetDivId}"]`);
+    if (targetDiv){
+      targetDiv.style.display='block';
+    }
+    else {
+      console.error('Target div not found');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  if(document.querySelectorAll('.action-btn').length) {
+    setupNavigationButtons();
+  }
+  if(document.querySelectorAll('.content').length){
+    showTargetDiv();
+  }
+})
