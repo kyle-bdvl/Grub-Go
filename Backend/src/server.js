38f45feba0a76184facdd85b929b2b4af2260f99
Kyle <kyle.boudville@gmail.com>
2025-01-20 00:11:44 +0800

    got the entire app to be on the server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,70 +1,47 @@
+const express = require("express");
+const path = require("path");
 
-import bodyParser from "body-parser";
-import express from "express";
-import {dirname} from "path";
-import {fileURLToPath} from "url";
-const __dirname= dirname(fileURLToPath(import.meta.url));
-
-// const express = require("express");
-// const path = require("path");
 const app = express();
 const PORT = 3000;
 
 // Middleware to serve static files (for Pictures and Icons)
-
 // Middleware to serve static files (CSS, JS)
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.get("/index.html", (req,res)=>{
-  res.sendFile(__dirname + "../../Front-end/Amena");
-});
-
-app.post("/login", (req, res) =>{
-  console.log(req.body);
-});
-
-
 
 app.use(express.static(path.join(__dirname, "../../Front-end/Amena")));
-
 app.use(express.static(path.join(__dirname, "../../Front-end/Kyle")));
 
-app.use(express.static(path.join(__dirname, "../../Front-end/Pictures")));
-
-app.use(express.static(path.join(__dirname, "../../Front-end/Icons")));
-
 app.use(express.static(path.join(__dirname, "../../Front-end/Ewing")));
-
 app.use(express.static(path.join(__dirname, "../../Front-end/Karthik")));
-
 app.use(express.static(path.join(__dirname, "../../Front-end/Abinash")));
 
-Route to serve the HTML file
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../Front-end/Amena/index.html"));
+// Route to serve the HTML file
+app.get("/index.html", (req, res) => {
+  
+  res.sendFile(path.resolve(__dirname, "../../Front-end/Amena"));
 });
+
+app.use(express.static(path.join(__dirname, "../../Front-end")));
+// app.use(express.static(path.join(__dirname, "../../Front-end/Icons")));
+
 
 app.get("/Kyle/restaurant.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Kyle/restaurant.html"));
 });
 
-
 app.get("/Kyle/mealPage.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Kyle/mealPage.html"));
 });
-
 app.get("/Ewing/cart.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Ewing/cart.html"));
 });
-
 app.get("/Ewing/payment.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Ewing/payment.html"));
 });
 
+// Start the server
 app.get("/Karthik/delivery.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Karthik/delivery.html"));
 });
-
 app.get("/Abinash/Communication", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../Front-end/Abinash/Communication.html"));
 });
